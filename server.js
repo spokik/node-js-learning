487d229201eb1b0a7f0e01fb109483f35e6f5a97
Spokik <Tackesi@yandex.ru>
2022-04-26 23:16:21 +0300

    test commit

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log(123);
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json("nu vse ok");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started ${PORT} port - http://localhost:5000/ `);
+});
